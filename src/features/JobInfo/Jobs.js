e23c92068936f91ad140847381451e51a0877975
Sunwoo <swjeong0825@gmail.com>
2021-01-23 23:55:02 +0900

    Sunwoo first commit

--- src/features/JobInfo/Jobs.js
+++ src/features/JobInfo/Jobs.js
@@ -14,23 +14,38 @@ export function Jobs() {
   const rectangleClass = "w-96 h-96 m-10 "
   const center = " mx-auto w-10/12 mt-14 "
   const bgColorWithApplicationStep = {
-    "0": " bg-yellow-500 ",
-    "1": " bg-yellow-200 ",
-    "2": " bg-green-200 ",
-    "3": " bg-green-400 ",
-    "4": " bg-red-500 "
+    "0": "h-5 w-1/4 bg-yellow-500 ",
+    "1": "h-5 w-1/2 bg-yellow-200 ",
+    "2": "h-5 w-3/4 bg-green-200 ",
+    "3": "h-5 w-full bg-green-500 ",
+    "4": "h-5 w-full bg-red-500 "
   }
 
   const jobsObjToJSX = jobs.map(job => {
       return (
-        <div id = {job.ID} className = {rectangleClass + bgColorWithApplicationStep[4]}>
+        <div _id = {job._id} className = {rectangleClass + "bg-gray-800"}>
+                    
             <div className = "float-right">
-                <GrClose className = "m-3"/>
+                <GrClose className = "m-3 bg-gray-100"/>
             </div>
-            <div className = {"h-48 bg-white" + center}>
-                asfaf
+            <div className = {"h-48 bg-white" + center +  "pt-4"}>
+                <div className = "text-center p-4 text-3xl">
+                    {job.companyName}
+                </div>
+                <div className = "text-center p-2 text-xl">
+                    {job.jobTitle}
+                </div>
+                <div className = "text-center p-2 text-base">
+                    {job.category}
+                </div>
             </div>
-        </div>
+            <div className = "mt-28 mx-3">
+                <div className = {bgColorWithApplicationStep[job.applicationStep] + "w-full bg-opacity-30"}>
+                    <div className = {bgColorWithApplicationStep[job.applicationStep]}></div>
+                </div>
+            </div>
+        
+        </div>         
       )
   })
 
@@ -41,9 +56,11 @@ export function Jobs() {
         </div>
         <div className = "mx-16 my-12">
             <div className = "flex flex-wrap justify-evenly">
-                <div className = {rectangleClass + "bg-gray-800"}>
+                {jobsObjToJSX}
+                {/* <div className = {rectangleClass + "bg-gray-800"}>
+
                     <div className = "float-right">
-                        <GrClose className = "m-3"/>
+                        <GrClose className = "m-3 bg-gray-100"/>
                     </div>
                     <div className = {"h-48 bg-white" + center +  "pt-4"}>
                         <div className = "text-center p-4 text-3xl">
@@ -57,11 +74,12 @@ export function Jobs() {
                         </div>
                     </div>
                     <div className = "mt-28 mx-3">
-                        <div >
-                            <div className = {bgColorWithApplicationStep[4] + "w-1/4 h-5"}></div>
+                        <div className = {bgColorWithApplicationStep[0] + "w-full bg-opacity-30"}>
+                            <div className = {bgColorWithApplicationStep[0]}></div>
                         </div>
                     </div>
-                </div>               
+                    
+                </div>                */}
             </div>
         </div>
     </div>
